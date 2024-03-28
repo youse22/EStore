1e0a7f3c29f31599472ef986f2edf961dc19ed53
Yousenie Dorsainvil <109559322+youse22@users.noreply.github.com>
2024-03-27 20:35:56 -0400

    moncash payment integration with views.py

--- Estore/views.py
+++ Estore/views.py
@@ -32,10 +32,10 @@ def login(request):
             password = form.cleaned_data['password']
             user = authenticate(request, username=username, password=password)
             if user is not None:
-                auth_login(request, user)  
-                return redirect('product_page')  
-        else:
-            form = AuthenticationForm()
+                auth_login(request, user)
+                return redirect('product_page')
+    else:
+        form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
 
 
@@ -72,11 +72,12 @@ def cart_page(request):
 
     return render(request, 'cart_page.html', {'cart_items': cart_items, 'total_price': total_price})
 
+
 def update_cart(request, product_id):
     if request.method == 'POST':
         new_quantity = int(request.POST.get('quantity'))
-        cart = request.session.get('cart', {})
-        cart[str(product_id)] = new_quantity
+        cart = request.session.get('cart', {})  
+        cart[product_id] = new_quantity  
         request.session['cart'] = cart
     return redirect('cart_page')
 
@@ -99,26 +100,21 @@ def checkout(request):
 
     if request.method == 'POST':
         order_id='es001'
-        client_id = ' 481f4da5a814079ab9cddd2a6435efc8'
+        client_id = '481f4da5a814079ab9cddd2a6435efc8'
         client_secret = 'oHrr4tbnB1PH0uz6VQNUve6i4a2aTk99VKjprl1AdE_HmynTAKY5M8pZRU7V--Pv'
         moncash = moncashify.API(client_id, client_secret)
         print('hello')
         payment = moncash.payment(order_id, total_price)
         print("hel")
         print(payment)
-        return payment.get_redirect_url()
+        return redirect(payment.redirect_url)
     else:
         return render(request, 'checkout.html', {'order_id': order_id, 'total_price': total_price})
 
 
 
 def confirmation_page(request):
-    order_id = request.session.get('order_id')
-    try:
-        order = Order.objects.get(id=order_id)
-    except Order.DoesNotExist:
-        return render(request, 'error_page.html', {'error_message': 'Order not found'})
-    return render(request, 'confirmation_page.html', {'order': order})
+    return render(request, 'confirmation_page.html')
 
 def error_page(request, error_code):
     error = ErrorPage.objects.get(error_code=error_code)
